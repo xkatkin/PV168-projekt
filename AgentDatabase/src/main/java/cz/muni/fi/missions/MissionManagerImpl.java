1ae7717210b2c6dcc9d6ae29fbae8b65f8ec3a00
Samuel Gorta <xgorta@fi.muni.cz>
2018-03-10 20:44:15 +0100

    MissionManagerImpl

---
+++ AgentDatabase/src/main/java/cz/muni/fi/missions/MissionManagerImpl.java
@@ -0,0 +1,28 @@
+package cz.muni.fi.missions;
+
+import java.util.List;
+
+public class MissionManagerImpl {
+    private long idCounter = 0;
+
+    public Mission createMission(Mission mission){
+        throw new UnsupportedOperationException("no implementation");
+    }
+
+    public Mission updateMission(Mission mission){
+        throw new UnsupportedOperationException("no implementation");
+    }
+
+    public boolean deleteMission(long MissionId){
+        throw new UnsupportedOperationException("no implementation");
+    }
+
+    public Mission findMissionByid(long missionId){
+        throw new UnsupportedOperationException("no implementation");
+    }
+
+    List<Mission> findAllMissions(){
+        throw new UnsupportedOperationException("no implementation");
+    }
+
+}
