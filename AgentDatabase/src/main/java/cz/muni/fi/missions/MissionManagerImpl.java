e3a68d6cee66847e2f85f26b74bc6a6fd0d5eddd
Samuel Gorta <xgorta@fi.muni.cz>
2018-03-21 20:55:24 +0100

    missionManagerImpl

--- AgentDatabase/src/main/java/cz/muni/fi/missions/MissionManagerImpl.java
+++ AgentDatabase/src/main/java/cz/muni/fi/missions/MissionManagerImpl.java
@@ -9,6 +9,7 @@ import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 
 import javax.sql.DataSource;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 /**
@@ -17,20 +18,28 @@ import java.util.List;
 public class MissionManagerImpl implements MissionManager{
     private JdbcTemplate jdbc;
 
-    public MissionManagerImpl(DataSource dataSource) {
+    MissionManagerImpl(DataSource dataSource) {
         this.jdbc = new JdbcTemplate(dataSource);
     }
 
+    private DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("d MMM yyyy");
 
+    @Override
     public void createMission(Mission mission){
-        //TODO: if-y
+        if (mission == null){
+            throw new IllegalArgumentException("Argument mission is null.");
+        }
+        if (mission.getTarget() == null || mission.getDeadline() == null){
+            throw new IllegalArgumentException("Mission with illegal atributes.");
+        }
+
         SimpleJdbcInsert insertMission = new SimpleJdbcInsert(jdbc)
                 .withTableName("missions").usingGeneratedKeyColumns("id");
 
         SqlParameterSource parameters = new MapSqlParameterSource()
                 .addValue("target", mission.getTarget())
                 .addValue("necessaryEquipment", mission.getNecesarryEquipment())
-                .addValue("deadline", mission.getDeadline());
+                .addValue("deadline", dateFormatter.format(mission.getDeadline()));
 
         Number id = insertMission.executeAndReturnKey(parameters);
         mission.setId(id.longValue());
@@ -39,27 +48,31 @@ public class MissionManagerImpl implements MissionManager{
     private RowMapper<Mission> missionMapper = (rs, rowNum) ->
             new Mission(rs.getLong("id"),
                     rs.getString("target"),
-                    Enum.valueOf(Equipment, rs.getString("necessaryEquipment")),
-                    LocalDate.of);
+                    Enum.valueOf(Equipment.class, rs.getString("necessaryEquipment")),
+                    LocalDate.parse(rs.getString("deadline"), dateFormatter));
 
+    @Override
     public void updateMission(Mission mission){
         jdbc.update("UPDATE missions set target=?,necessaryEquipment=?,deadline=? where id=?",
                 mission.getTarget(),
                 mission.getNecesarryEquipment(),
-                mission.getDeadline(),
+                dateFormatter.format(mission.getDeadline()),
                 mission.getId());
     }
 
+    @Override
     public boolean deleteMission(long missionId){
-        return jdbc.update("DELETE FROM customers WHERE id=?", missionId) == 1;
+        return jdbc.update("DELETE FROM mission WHERE id=?", missionId) == 1;
     }
 
+    @Override
     public Mission findMissionByid(long missionId){
-        throw new UnsupportedOperationException("no implementation");
+        return jdbc.queryForObject("SELECT * FROM mission WHERE id=?", missionMapper, missionId);
     }
 
+    @Override
     public List<Mission> findAllMissions(){
-        throw new UnsupportedOperationException("no implementation");
+        return jdbc.query("SELECT * FROM mission", missionMapper);
     }
 
 }
