3a0e9833282b064471a20b6ab4f967162227a16c
Samuel Gorta <xgorta@fi.muni.cz>
2018-04-09 21:46:59 +0200

    pekny vecer

--- webdragon/_web1/src/main/java/cz/muni/fi/pv168/web/Servlet.java
+++ webdragon/_web1/src/main/java/cz/muni/fi/pv168/web/Servlet.java
@@ -1,6 +1,9 @@
 
 package cz.muni.fi.pv168.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,30 +13,96 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Enumeration;
 
-@WebServlet(urlPatterns = {"/muj/*", "*.muj"})
-public class Servlet extends HttpServlet {
+
+/**
+ * Servlet for managing books.
+ */
+@WebServlet(BooksServlet.URL_MAPPING + "/*")
+public class BooksServlet extends HttpServlet {
+
+    private static final String LIST_JSP = "/missionList.jsp";
+    public static final String URL_MAPPING = "/books";
+
+    private final static Logger log = LoggerFactory.getLogger(BooksServlet.class);
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        showBooksList(request, response);
+    }
 
-        response.setContentType("text/html;charset=UTF-8");
-        PrintWriter out = response.getWriter();
-        out.println("<h1>Muj prvni servlet</h1>");
-
-        int n = 10;
-        String np = request.getParameter("n");
-        if (np != null) n = Integer.parseInt(np);
-
-        n *= 2;
-        for (int i = 0; i < n; i++) {
-            out.println("i=" + i + "<br/>");
-        }
-
-        Enumeration<String> headernames = request.getHeaderNames();
-        while (headernames.hasMoreElements()){
-            String header = headernames.nextElement();
-
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //aby fungovala čestina z formuláře
+        request.setCharacterEncoding("utf-8");
+        //akce podle přípony v URL
+        String action = request.getPathInfo();
+        switch (action) {
+            case "/add":
+                //načtení POST parametrů z formuláře
+                String name = request.getParameter("name");
+                String author = request.getParameter("author");
+                //kontrola vyplnění hodnot
+                if (name == null || name.length() == 0 || author == null || author.length() == 0) {
+                    request.setAttribute("chyba", "Je nutné vyplnit všechny hodnoty !");
+                    showBooksList(request, response);
+                    return;
+                }
+                //zpracování dat - vytvoření záznamu v databázi
+                try {
+                    Book book = new Book(null, name, author);
+                    getBookManager().createBook(book);
+                    log.debug("created {}",book);
+                    //redirect-after-POST je ochrana před vícenásobným odesláním formuláře
+                    response.sendRedirect(request.getContextPath()+URL_MAPPING);
+                    return;
+                } catch (BookException e) {
+                    log.error("Cannot add book", e);
+                    response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
+                    return;
+                }
+            case "/delete":
+                try {
+                    Long id = Long.valueOf(request.getParameter("id"));
+                    getBookManager().deleteBook(id);
+                    log.debug("deleted book {}",id);
+                    response.sendRedirect(request.getContextPath()+URL_MAPPING);
+                    return;
+                } catch (BookException e) {
+                    log.error("Cannot delete book", e);
+                    response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
+                    return;
+                }
+            case "/update":
+                //TODO
+                return;
+            default:
+                log.error("Unknown action " + action);
+                response.sendError(HttpServletResponse.SC_NOT_FOUND, "Unknown action " + action);
         }
     }
+
+    /**
+     * Gets BookManager from ServletContext, where it was stored by {@link StartListener}.
+     *
+     * @return BookManager instance
+     */
+    private BookManager getBookManager() {
+        return (BookManager) getServletContext().getAttribute("bookManager");
+    }
+
+    /**
+     * Stores the list of books to request attribute "books" and forwards to the JSP to display it.
+     */
+    private void showBooksList(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            request.setAttribute("books", getBookManager().getAllBooks());
+            request.getRequestDispatcher(LIST_JSP).forward(request, response);
+        } catch (BookException e) {
+            log.error("Cannot show books", e);
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
+        }
+    }
+
+}
 
 }
--- webdragon/_web1/src/main/java/cz/muni/fi/pv168/web/StartListener.java
+++ webdragon/_web1/src/main/java/cz/muni/fi/pv168/web/StartListener.java
@@ -1,25 +1,34 @@
 package cz.muni.fi.pv168.web;
 
+
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+import org.slf4j.Logger;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @WebListener
 public class StartListener implements ServletContextListener {
 
+    final static Logger log = LoggerFactory.getLogger(StartListener.class);
+
     @Override
     public void contextInitialized(ServletContextEvent ev) {
-        System.out.println("aplikace inicializována");
+        log.info("aplikace inicializována");
         ServletContext servletContext = ev.getServletContext();
-        String serverInfo = servletContext.getServerInfo();
-        servletContext.log("serevrInfo = " + serverInfo);
-
-        servletContext.setAttribute("key", "value");
+        ApplicationContext springContext = new AnnotationConfigApplicationContext(SpringConfig.class);
+        servletContext.setAttribute("agentManager", springContext.getBean("agentManager", AgentManagerImpl.class));
+        servletContext.setAttribute("missionManager", springContext.getBean("missionManager", MissionManagerImpl.class));
+        servletContext.setAttribute("contractManager", springContext.getBean("contractManager", ContractManagerImpl.class));
+        log.info("vytvořeny manažery");
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent ev) {
-        System.out.println("aplikace končí");
+        log.info("aplikace končí");
     }
 }
